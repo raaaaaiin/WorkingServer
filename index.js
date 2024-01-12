5a09c7c4617ea4eb299ddf3d5f119aca15321ed2
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2024-01-12 14:08:36 +0800

    Added simple routes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3000;
+
+
+app.post('/sign-jwt', (req, res) => {
+  
+});
+
+
+app.get('/validate-and-redirect', (req, res) => {
+  
+});
+
 
 app.listen(port, () => {
   console.log(`JWT Server is running on port ${port}`);
