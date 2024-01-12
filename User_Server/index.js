b97d0380cf72e4297c20f73e135a650ddfd7fadd
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2024-01-12 14:21:24 +0800

    Created basic routing without function for user_server

--- User_Server/index.js
+++ User_Server/index.js
@@ -1,6 +1,42 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('./db'); 
+
 const app = express();
 const port = 4000;
+
+function validateJwt(req, res, next) {
+
+}
+
+
+app.use(express.json());
+
+
+app.post('/users', validateJwt, (req, res) => {
+  
+});
+
+
+app.get('/users', validateJwt, (req, res) => {
+ 
+});
+
+
+app.get('/users/:username', validateJwt, (req, res) => {
+  
+});
+
+
+app.put('/users/:username', validateJwt, (req, res) => {
+ 
+});
+
+
+app.delete('/users/:username', validateJwt, (req, res) => {
+  
+});
+
 
 app.listen(port, () => {
   console.log(`User Management Server is running on port ${port}`);
