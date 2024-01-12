8e563b6f87741a6f5ee1b818f416a5c1a704f6b7
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2024-01-12 14:24:54 +0800

    added the validate jwt on User_server

--- User_Server/index.js
+++ User_Server/index.js
@@ -6,8 +6,21 @@ const app = express();
 const port = 4000;
 
 function validateJwt(req, res, next) {
-
-}
+    const token = req.headers.authorization;
+  
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized - JWT missing' });
+    }
+  
+    jwt.verify(token, 'marcraineerfilosopo06272000', (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Unauthorized - Invalid JWT' });
+      }
+  
+      req.jwtPayload = decoded;
+      next();
+    });
+  }
 
 
 app.use(express.json());
